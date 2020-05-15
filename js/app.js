c9e21f17444b5310dee42deeb9f076c584919617
Korpan-Artem <arttodoc@gmail.com>
2020-05-15 10:47:18 +0300

    Опять PWA - не получилось

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
-window.addEventListener('load',()=>{
-
     if('serviceWorker' in navigator){
-        navigator.serviceWorker.register('.sw.js')
-            .then(registration =>{
-                console.log('Service worker successfully registration',registration);
-            })
-            .catch(error =>{
-                console.log('Service worker registration failed',error);
-            });
+        navigator.serviceWorker.register("/js/sw.js")
+            .then((reg)=> console.log('service worker registered',reg))
+            .catch((err)=> console.log('service worker not registered',err));
     }
-});
