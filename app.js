01f946ffd103d727298b4a857e47ddf1efea1d08
Korpan-Artem <arttodoc@gmail.com>
2020-05-25 16:39:38 +0300

    Ничего не получается

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator) {
     try{
-        navigator.serviceWorker.register('sw.js');
+        navigator.serviceWorker.register('js/sw.js');
         console.log(`SW registered`);
     }catch (error) {
         console.log(`SW reg failed`);
