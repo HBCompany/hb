fa6a4bd22e26a8ddea916fc53173c93708b12d59
Korpan-Artem <arttodoc@gmail.com>
2020-05-25 15:59:16 +0300

    Добавил сервисворкер

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static';
+/*const staticCacheName = 'site-static';
 const assets = [
     '/',
     '/index.html',
@@ -15,7 +15,7 @@ self.addEventListener('install', evt => {
         })
     );
 });
- 
+
 self.addEventListener('activate', evt => {
     //console.log('service worker has been activated');
 });
@@ -23,4 +23,29 @@ self.addEventListener('activate', evt => {
 //fetch event
 self.addEventListener('fetch', evt => {
     //console.log('fetch event',evt);
+});const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/css/style.css',
+    '/img/setting.png'
+];
+self.addEventListener('install', evt => {
+    //console.log('service worker has been installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache=>{
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
+
+self.addEventListener('activate', evt => {
+    //console.log('service worker has been activated');
+});
+
+//fetch event
+self.addEventListener('fetch', evt => {
+    //console.log('fetch event',evt);
+});*/
