e69cf8dc35a584c8af026a596bce8ee6837bc0cc
Korpan-Artem <arttodoc@gmail.com>
2020-06-09 14:27:31 +0300

    #3

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    // You can save the event in order to trigger the prompt later (see below)
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
     deferredPrompt = e;
-
-    // Update UI notify the user they can add to home screen.
-    // With this you can show the Add to home screen button.
+    // Update UI notify the user they can install the PWA
     showInstallPromotion();
 });
 
-btnAdd.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    btnAdd.style.display = 'none';
-
-    // Show the prompt
+buttonInstall.addEventListener('click', (e) => {
+    // Hide the app provided install promotion
+    hideMyInstallPromotion();
+    // Show the install prompt
     deferredPrompt.prompt();
-
     // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the install prompt');
+        } else {
+            console.log('User dismissed the install prompt');
+        }
+    });
 });
