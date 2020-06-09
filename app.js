bae2d3cfc00163609c246924567f84b701e52964
Korpan-Artem <arttodoc@gmail.com>
2020-06-09 12:53:36 +0300

    №1

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt
-    e.preventDefault();
-    // Stash the event so it can be triggered later.
+    // You can save the event in order to trigger the prompt later (see below)
     deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen
-    addBtn.style.display = 'block';
 
-    addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
+    // Update UI notify the user they can add to home screen.
+    // With this you can show the Add to home screen button.
+    showInstallPromotion();
+});
+
+btnAdd.addEventListener('click', (e) => {
+    // hide our user interface that shows our A2HS button
+    btnAdd.style.display = 'none';
+
+    // Show the prompt
+    deferredPrompt.prompt();
+
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice
+        .then((choiceResult) => {
             if (choiceResult.outcome === 'accepted') {
                 console.log('User accepted the A2HS prompt');
             } else {
@@ -22,5 +26,4 @@ window.addEventListener('beforeinstallprompt', (e) => {
             }
             deferredPrompt = null;
         });
-    });
 });
